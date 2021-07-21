30f2d8100d4ac0f6c9961d0e0b6b7c47827431f0
karan <31614730+knuo@users.noreply.github.com>
2021-07-21 13:51:05 +0530

    Update CovidData EDA.sql

--- CovidData EDA.sql
+++ CovidData EDA.sql
@@ -18,7 +18,6 @@ order by 1,2
 
 
 -- Total Cases vs Total Deaths
--- Shows likelihood of dying if you contract covid in your country
 
 Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
